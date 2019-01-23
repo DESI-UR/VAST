Fixed unit conversion in classifyEnvironment.py, added Mask_galaxy_plot

classifyEnvironment.py
@@ -26,6 +26,8 @@ c = 3e5 # km/s
 h = 1
 H = 100*h
 
+DtoR = np.pi/180
+
 
 ################################################################################
 #
@@ -44,8 +46,10 @@ voids['voidID'] == index number identifying to which void the sphere belongs
 '''
 
 #galaxy_file = input('Galaxy data file (with extension): ')
-#galaxy_file = '/Users/kellydouglass/Documents/Research/Rotation_curves/vflag_not_found.txt'
-galaxy_file = '/Users/kellydouglass/Documents/Research/Rotation_curves/vflag_not_classified.txt'
+galaxy_file = '/Users/kellydouglass/Documents/Research/Rotation_curves/vflag_not_found.txt'
+#galaxy_file = '/Users/kellydouglass/Documents/Research/Rotation_curves/vflag_not_classified.txt'
+#galaxy_file = '/Users/kellydouglass/Documents/Research/Rotation_curves/void_reclassification.txt'
+#galaxy_file = '/Users/kellydouglass/Documents/Research/Rotation_curves/wall_reclassification.txt'
 galaxies = Table.read(galaxy_file, format='ascii.ecsv')
 
 
@@ -61,13 +65,13 @@ galaxies = Table.read(galaxy_file, format='ascii.ecsv')
 galaxies_r = c*galaxies['NSA_zdist']/H
 
 # Calculate x-coordinates
-galaxies_x = galaxies_r*np.cos(galaxies['NSA_DEC'])*np.cos(galaxies['NSA_RA'])
+galaxies_x = galaxies_r*np.cos(galaxies['NSA_DEC']*DtoR)*np.cos(galaxies['NSA_RA']*DtoR)
 
 # Calculate y-coordinates
-galaxies_y = galaxies_r*np.cos(galaxies['NSA_DEC'])*np.sin(galaxies['NSA_RA'])
+galaxies_y = galaxies_r*np.cos(galaxies['NSA_DEC']*DtoR)*np.sin(galaxies['NSA_RA']*DtoR)
 
 # Calculate z-coordinates
-galaxies_z = galaxies_r*np.sin(galaxies['NSA_DEC'])
+galaxies_z = galaxies_r*np.sin(galaxies['NSA_DEC']*DtoR)
 
 
 ################################################################################
@@ -94,4 +98,4 @@ for i in range(len(galaxies)):
 # Output file name
 outfile = galaxy_file[:-4] + '_vflag.txt'
 
-galaxies.write(outfile, format='ascii.ecsv')
+galaxies.write(outfile, format='ascii.ecsv', overwrite=True)

vflag.py
@@ -23,6 +23,20 @@ maskdec = 180
 dec_offset = -90
 
 
+
+
+# Read in survey mask
+survey_mask = Table.read('SDSSdr7/cbpdr7mask.dat', format='ascii.commented_header')  # SDSS DR7
+
+# Make mask
+mask = np.zeros((maskra, maskdec), dtype=np.bool)
+mask[survey_mask['ra'].astype(int), survey_mask['dec'].astype(int) - dec_offset] = True
+
+# Distance limits (units of Mpc/h, taken from voids_sdss.py)
+rmin = 0
+rmax = 300
+
+
 ################################################################################
 #
 #   FUNCTION - DETERMINE_VFLAG
@@ -40,14 +54,14 @@ def determine_vflag(x,y,z,voids):
     distance_to_center = np.sqrt((voids['x'] - x)**2 + (voids['y'] - y)**2 + (voids['z'] - z)**2)
     
     # Boolean to find which void surrounds the galaxy, if any
-    bool = distance_to_center < voids['radius']
+    boolean = distance_to_center < voids['radius']
     
     
     ############################################################################
     #   VOID GALAXIES
     ############################################################################
     
-    if any(bool):
+    if any(boolean):
         # The galaxy resides in at least one void
         vflag = 1
         
@@ -63,21 +77,10 @@ def determine_vflag(x,y,z,voids):
         # Is the galaxy outside the survey boundary?
         ########################################################################
 
-        # Read in survey mask
-        survey_mask = Table.read('SDSSdr7/cbpdr7mask.dat', format='ascii.commented_header')  # SDSS DR7
-
-        # Make mask
-        mask = np.zeros((maskra, maskdec), dtype=np.bool)
-        mask[survey_mask['ra'].astype(int), survey_mask['dec'].astype(int) - dec_offset] = True
-
-        # Distance limits (units of Mpc/h, taken from voids_sdss.py)
-        rmin = 0
-        rmax = 300
-
         coord_array = np.array([[x,y,z]])
 
         # Check to see if the galaxy is within the survey
-        if not_in_mask(np.array([[x,y,z]]), mask, rmin, rmax):
+        if not_in_mask(coord_array, mask, rmin, rmax):
             # Galaxy is outside the survey mask
             vflag = -9
 