22816cace9c11608ef39b096eaac4dbafe42fe63
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2019-01-23 12:50:59 -0500

    Package init.

---
+++ python/VoidFinder/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD-style license - see LICENSE.
+# -*- coding: utf-8 -*-
+"""
+==========
+VoidFinder
+==========
+Implementation of the galaxy VoidFinder algorithm by Hoyle and Vogeley (2002),
+based on the algorithm described by El-Ad and Piran (1997).
+"""
+from __future__ import absolute_import
+from ._version import __version__
