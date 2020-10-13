ac2361d8048e1bad49f76d98f8b0872f4b9dfabc
Kelly <kad347@drexel.edu>
2020-10-13 17:15:49 -0400

    Fixed version import in VoidFinder

--- VoidFinder/python/setup.py
+++ VoidFinder/python/setup.py
@@ -29,7 +29,7 @@ setup_keywords['author'] = 'Kelly Douglass, University of Rochester'
 setup_keywords['author_email'] = 'kellyadouglass@rochester.edu'
 setup_keywords['license'] = 'BSD'
 setup_keywords['url'] = 'https://github.com/DESI-UR/Voids/VoidFinder'
-setup_keywords['version'] = get_version()
+setup_keywords['version'] = '0.1.1.dev3'
 setup_keywords['install_requires'] = ['cython',
                                       'h5py',
                                       'psutil',
