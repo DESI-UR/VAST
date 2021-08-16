05b609d7a307beee430b23d6b59bdbf15d50e017
Kelly <kad347@drexel.edu>
2021-08-16 19:25:06 -0400

    Adding sys.path print for debugging

--- Documentation/source/conf.py
+++ Documentation/source/conf.py
@@ -12,9 +12,10 @@
 
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../../VoidFinder/'))
-#sys.path.insert(1, os.path.abspath('../../Vsquared/'))
-sys.path.insert(0, os.path.abspath('../../VoidFinder/vast/voidfinder/'))
+sys.path.insert(0, os.path.abspath('../../VoidFinder/'))
+sys.path.insert(1, os.path.abspath('../../Vsquared/'))
+#sys.path.insert(0, os.path.abspath('../../VoidFinder/vast/voidfinder/'))
+print(sys.path)
 
 # -- Project information -----------------------------------------------------
 
