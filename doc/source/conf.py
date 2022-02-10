18e66eac375e388160d90fb1696328634ce142bd
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2022-02-09 20:48:41 -0500

    Enable sphinx_rtd_theme.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -8,6 +8,8 @@ import sys
 import os
 import re
 
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../../python/'))
 import vast
 
@@ -36,6 +38,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
     'numpydoc',
     'sphinxarg.ext'
     #'pyquickhelper.sphinxext.sphinx_autosignature'
@@ -58,7 +61,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
