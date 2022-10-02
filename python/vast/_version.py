0cb93faa39f239c6d5e32f8953465b7fbe5f47e9
QuiteAFoxtrot <soneill5045@gmail.com>
2022-10-02 15:46:02 -0400

    Updated versioning guidance in _version.py to something more formaliz… (#88)
    * Updated versioning guidance in _version.py to something more formalized and cleaned up a number of the cython warnings when building
    
    * forgot to increment version number

--- python/vast/_version.py
+++ python/vast/_version.py
@@ -1,20 +1,18 @@
 
 """
 
-Hopefully this makes sense to everyone - trying to stick with the fairly common
-'major.minor.revision' GNU version number format here.  As a rule of thumb,
-major is incremented when we make any huge updates (maybe adding a whole new
-algorithm, or a complete re-write of an existing one), the minor
-is incremented when we make any significant or backwards-incompatible changes,
-and the 3rd number is for more general bug fixes, enhancements, quality
-changes and so forth.
+Found a better reference for versioning:
 
-Note that this scheme is still pretty loosey-goosey and ultimately it still falls
-on the package maintainers to debate and find consensus on each issue individually
-but this can provide a useful frame of reference for the versioning.
+https://semver.org/
 
-Also when we hit something like 1.9.x we dont have to go to 2.0.0, we can go to 1.10.x
-just in case anyone wondered about that like I used to.
+Given a version number MAJOR.MINOR.PATCH, increment the:
+
+MAJOR version when you make incompatible API changes
+MINOR version when you add functionality in a backwards compatible manner
+PATCH version when you make backwards compatible bug fixes
+Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+
+
 """
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
