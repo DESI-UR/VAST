Bug fix for V2 colum units

python/vast/vsquared/zobov.py
@@ -712,16 +712,18 @@ class Zobov:
                     names = names,
                     units = ['','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h'])
         else:
+            """
             names = ['x','y','z','redshift','ra','dec','radius','x1','y1','z1','x2','y2','z2','x3','y3','z3']
             if self.capitalize:
                 names = [name.upper() for name in names]
+            """
             vz,vra,vdec = toSky(self.vcens,self.H0,self.Om_m,self.zstep)
             columns = [np.arange(len(self.vrads)), vcen[0], vcen[1], vcen[2], 
                        vz, vra, vdec, self.vrads,  
                        vax1[0], vax1[1], vax1[2], vax2[0], vax2[1], vax2[2], vax3[0], vax3[1], vax3[2]]
             names = ['void','x','y','z',
                      'redshift','ra','dec','radius','x1','y1','z1','x2','y2','z2','x3','y3','z3']
-            units = ['','Mpc/h','Mpc/h','Mpc/h','deg','deg','Mpc/h', '', 'Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h']
+            units = ['','Mpc/h','Mpc/h','Mpc/h','','deg','deg','Mpc/h', 'Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h','Mpc/h']
 
             if self.visualize:
                 columns += [self.varea_t,self.varea_0]