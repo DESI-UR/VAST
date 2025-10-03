f9028ec38616588f63a328c1ee83bb4d4ea65c62
QuiteAFoxtrot <soneill5045@gmail.com>
2025-10-03 10:12:45 -0400

    Minor fixes to new pip install machinery and added Steve's email addr

--- setup.py
+++ setup.py
@@ -5,14 +5,82 @@
 import os
 import codecs
 from glob import glob
+
+import re
+from os.path import abspath, exists, isdir, isfile, join
+
 #
 from distutils.command.sdist import sdist as DistutilsSdist
 from setuptools import setup, dist, find_packages
 from setuptools.extension import Extension
 from setuptools.command.build_ext import build_ext as _build_ext
-#
-from python.vast._git import get_version
-#
+
+
+# Can't do 'rom python.vast._git import get_version' since 'python' is just a folder in our
+# repository, and not a python package itself
+# Instead, moving the two utility functions from the _git.py module directly here since
+# this is the only place they are used anyway, they are simple, and Segev wrote nice
+# docstrings.  If we need some more complex get_version() machinery we can revisit this 
+# at that time.
+# from python.vast._git import get_version
+
+"""Some code for interacting with git.
+"""
+
+def get_version():
+    """Get the value of ``__version__`` without having to import the package.
+
+    Returns
+    -------
+    :class:`str`
+        The value of ``__version__``.
+    """
+    ver = 'unknown'
+    try:
+        version_dir = find_version_directory()
+    except IOError:
+        return ver
+
+    version_file = join(version_dir, '_version.py')
+    with open(version_file, 'r') as f:
+        for line in f.readlines():
+            mo = re.match("__version__ = '(.*)'", line)
+            if mo:
+                ver = mo.group(1)
+    return ver
+
+
+def find_version_directory():
+    """Return the name of a directory containing version information.
+    Looks for files in the following places:
+    * python/vast/_version.py
+    * vast/_version.py
+
+    Returns
+    -------
+    :class:`str`
+        Name of a directory that can or does contain version information.
+
+    Raises
+    ------
+    IOError
+        If no valid directory can be found.
+    """
+    packagename = 'vast'
+    setup_dir = abspath('.')
+
+    if isdir(join(setup_dir, 'python', packagename)):
+        version_dir = join(setup_dir, 'python', packagename)
+    elif isdir(join(setup_dir, packagename)):
+        version_dir = join(setup_dir, packagename)
+    else:
+        raise IOError('Could not find a directory containing version information!')
+    return version_dir
+
+
+
+
+
 # Begin setup
 #
 setup_keywords = dict()
