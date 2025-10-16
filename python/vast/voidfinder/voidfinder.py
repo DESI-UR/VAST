0339f1ada24f49fc2ced6a63a63dae9c46f520c4
Hernan Rincon <102341956+hbrincon@users.noreply.github.com>
2025-10-16 18:44:49 -0400

    Vf fast test (#139)
    * barebones version of voidfinder designed for speed on large simulation suites
    
    * Option for enabling maximal-spheres-only mode
    
    * Version updates
    
    * Added docstrings and shifted around some lines to not overwrite data
    
    * Removed erroneous comma
    
    * Adjusting to 80 character-long lines
    
    ---------
    
    Co-authored-by: Hernan Benjamin Rincon <hrincon@login18.chn.perlmutter.nersc.gov>
    Co-authored-by: Hernan Benjamin Rincon <hrincon@login08.chn.perlmutter.nersc.gov>
    Co-authored-by: Hernan Benjamin Rincon <hrincon@login13.chn.perlmutter.nersc.gov>
    Co-authored-by: Kelly Douglass <kellyadouglass@rochester.edu>

--- python/vast/voidfinder/voidfinder.py
+++ python/vast/voidfinder/voidfinder.py
@@ -790,6 +790,8 @@ def find_voids(galaxy_coords_xyz,
                print_after=5.0,
                capitalize_colnames = False,
                save_missing_galaxies = True,
+               SOCKET_PATH="/tmp/voidfinder.sock",
+               maximal_spheres_only = False
                ):
     """
     Main entry point for VoidFinder.  
@@ -1019,6 +1021,21 @@ def find_voids(galaxy_coords_xyz,
         If True, and if the galaxies have not been previously saved to the output 
         file during the the galaxy filtering stage, then galaxy input is saved 
         to the output
+
+    SOCKET_PATH : str
+        The socket path file used for multithreading, ending wiht a ".sock" file. 
+        Unique socket paths should be specified for simultaneous VoidFinder runs. 
+        Defaults to "/tmp/voidfinder.sock" Two unique socket paths are a used in 
+        each run, with the second socket path automatically appending a "2" to 
+        the first, e.g. "/tmp/voidfinder2.sock"
+
+    maximal_spheres_only : bool
+        If True, only maximal spheres are ourput by the algorithm. This mode is 
+        useful for runnning on large sets of thousands of mocks each of > Gpc 
+        sidelength, when the algoithm might otherwise be slowed down by hole
+        merging calculations.  Note that setting this to True also turns off the 
+        edge_void checker and only returns the Cartesian coordinates of the 
+        maximal spheres.
     
     
     Returns
@@ -1083,7 +1100,8 @@ def find_voids(galaxy_coords_xyz,
                                           batch_size=batch_size,
                                           verbose=verbose,
                                           print_after=print_after,
-                                          num_cpus=num_cpus)
+                                          num_cpus=num_cpus,
+                                          SOCKET_PATH=SOCKET_PATH)
 
     if verbose > 0:
         print('Found a total of', n_holes, 'potential voids.', flush=True)
@@ -1168,7 +1186,8 @@ def find_voids(galaxy_coords_xyz,
                                                pts_per_unit_volume=pts_per_unit_volume,
                                                num_surf_pts=20,
                                                num_cpus=num_cpus,
-                                               verbose=verbose)
+                                               verbose=verbose,
+                                               SOCKET_PATH=SOCKET_PATH[:-5]+'2'+SOCKET_PATH[-5:])
     
     if verbose > 0:
         print("Found", np.sum(np.logical_not(valid_idx)), "holes to cut", 
@@ -1209,7 +1228,8 @@ def find_voids(galaxy_coords_xyz,
                                                            boundary_hole, 
                                                            mask_checker,
                                                            min_maximal_radius=min_maximal_radius,
-                                                           verbose=verbose)
+                                                           verbose=verbose,
+                                                           maximal_spheres_only=maximal_spheres_only)
     
     if verbose > 0:
         print("Combine time:", time.time() - combine_start, flush=True)
@@ -1228,15 +1248,20 @@ def find_voids(galaxy_coords_xyz,
     myvoids_table['z'].unit='Mpc/h'
     myvoids_table['radius'].unit='Mpc/h'
 
-    maximal_spheres_table = xyz_to_radecz(maximal_spheres_table)
+    if not maximal_spheres_only:
+        # Only convert maximal sphere centers to sky coordinates when returning 
+        # full list of holes
+        maximal_spheres_table = xyz_to_radecz(maximal_spheres_table)
+
+        maximal_spheres_table['radius'].unit='Mpc/h'
+        maximal_spheres_table['r'].unit='Mpc/h'
+        maximal_spheres_table['ra'].unit='deg'
+        maximal_spheres_table['dec'].unit='deg'
 
     maximal_spheres_table['x'].unit='Mpc/h'
     maximal_spheres_table['y'].unit='Mpc/h'
     maximal_spheres_table['z'].unit='Mpc/h'
-    maximal_spheres_table['radius'].unit='Mpc/h'
-    maximal_spheres_table['r'].unit='Mpc/h'
-    maximal_spheres_table['ra'].unit='deg'
-    maximal_spheres_table['dec'].unit='deg'
+    
 
     if capitalize_colnames: 
 
