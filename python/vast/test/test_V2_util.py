490c6516c6fc3cebe6de542d149308199977c2b6
Hernan Benjamin Rincon <hrincon@login24.chn.perlmutter.nersc.gov>
2025-11-26 15:47:14 -0800

    test updates

--- python/vast/test/test_V2_util.py
+++ python/vast/test/test_V2_util.py
@@ -51,7 +51,7 @@ class TestV2Util(unittest.TestCase):
         c3 = np.array([0., 0., -100., 0.])
         coord = np.array([1000., 1000., 1000.]) + np.array([c1,c2,c3]).T
         ax2 = np.array([-12.3314653, 7.39154934, 4.1289785])
-        SMA = util.getSMA(10., coord, False, [-400,-400,-400], [400,400,400])
+        SMA = util.getSMA(10., [0., 0., 0.], coord, False, [-400,-400,-400], [400,400,400])
         self.assertTrue(np.isclose(SMA[1],ax2).all())
 
     def test_prob_false_voids(self):
