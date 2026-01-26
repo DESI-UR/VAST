4015e5e6f0304df366349e7a1e3ad56a4d6a1f78
Hernan Rincon <hbrincon2@gmail.com>
2026-01-26 13:49:31 -0500

    example scripts

---
+++ example_scripts/voboz.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+Driver program for Voronoi Voids (V^2) void finder module using the ZOBOV
+algorithm.
+"""
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from vast.vsquared.zobov import Zobov
+import cProfile
+
+p = ArgumentParser(description='Voronoi Voids (V^2) void finder.',
+                   formatter_class=ArgumentDefaultsHelpFormatter)
+
+p.add_argument('-m', '--method', default='0',
+               help='Void-finding method (0,1,2,...) or (VIDE,ZOBOV,ZOBOV2,...) or (vide,zobov,zobov2,...)')
+p.add_argument('-v', '--visualize', action='store_true', default=False,
+               help='Enable void visualization.')
+p.add_argument('-w', '--save_intermediate', action='store_true', default=False,
+               help='Save intermediate files in void calculation.')
+p.add_argument('-p', '--periodic', action='store_true', default=False,
+               help='Use periodic boundary contitions.')
+p.add_argument('-x', '--xyz', action='store_true', default=False,
+               help='Use cartesian coordinates.')
+
+req = p.add_argument_group('required named arguments')
+req.add_argument('-c', '--config', dest='config_file', required=True, default="DR7_config.ini",
+                 help='V^2 config file (INI format).')
+
+args = p.parse_args()
+
+
+if __name__ == "__main__":
+    
+    newZobov = Zobov(args.config_file, 
+                     #0, 3,
+                     #stages=[0,1,2,3],
+                     save_intermediate=args.save_intermediate,
+                     visualize=args.visualize,
+                     periodic=args.periodic,
+                     xyz=args.xyz,
+                     num_cpus=1,
+                     zones_mode='clusters',
+                     verbose=1)
+    
+    newZobov.sortVoids(method=args.method)
+    
+    newZobov.saveVoids()
+    
+    newZobov.saveZones()
+    
+    if args.visualize:
+        newZobov.preViz()
